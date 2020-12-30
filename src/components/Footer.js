870d67e0296af4026acd82fd1d34b944d9991566
Misha <mishayanenko@yandex.ru>
2020-12-30 14:47:58 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyrigth"> &#169; 2020 Mesto Russia</p>
-    </footer>
-  );
-}
